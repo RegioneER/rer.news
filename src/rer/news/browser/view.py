15e29b96c4fc0ec317b7173888207a83172b8db7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2017-05-16 08:50:14 +0200

    development template

--- src/rer/news/browser/view.py
+++ src/rer/news/browser/view.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from Products.Five.browser import BrowserView
 from zope.i18nmessageid import MessageFactory
 import pkg_resources
@@ -16,7 +17,7 @@ class ERNewsView(BrowserView):
 
     def getEffectiveDate(self):
         """
-        return a formatted date
+        return infos to format date
         """
         effective = self.context.effective()
         if effective.year() == 1969:
@@ -28,3 +29,25 @@ class ERNewsView(BrowserView):
             'day': effective.day(),
             'year': effective.year()
         }
+
+    def getNewsContents(self):
+        return self.context.getFolderContents()
+
+    def getLinks(self):
+        links = getattr(self.context, 'related_links', [])
+        if not links:
+            return []
+        return [x.to_object for x in links if self.canAccessLink(x)]
+
+    def canAccessLink(self, ref):
+        """
+        check if the reference is available for the current user or isn't a
+        broken reference.
+        """
+        if ref.isBroken():
+            return False
+        link = ref.to_object
+        return api.user.has_permission(
+            'View',
+            user=api.user.get_current(),
+            obj=link)
